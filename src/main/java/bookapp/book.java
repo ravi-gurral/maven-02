eda0b184c3328c9581827a3b7a15f71238b11453
Ravi <ravigurral@gmail.com>
2021-07-20 19:21:27 +0530

    second time

--- src/main/java/bookapp/book.java
+++ src/main/java/bookapp/book.java
@@ -5,6 +5,8 @@ public class book {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("book is empty");
+		String s="good night";
+		int i,j;
 
 	}
 
